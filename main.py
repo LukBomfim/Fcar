3fbb7dab6409dcce4ff4c7c1ebe9075f89c70781
LukBomfim <lucasbomfim2801@gmail.com>
2026-05-04 16:32:25 -0300

    criação do main e arquivo das rotas

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from backend.routes import routes
+
+app = Flask(__name__, 
+            template_folder='frontend/templates', 
+            static_folder='frontend/static')
+
+app.register_blueprint(routes)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
