53bf61e08759dae065a4c6cb55720ea44d0a7f0f
LukBomfim <lucasbomfim2801@gmail.com>
2026-05-28 13:34:22 -0300

    criação do spinner

--- frontend/static/js/contato.js
+++ frontend/static/js/contato.js
@@ -2,6 +2,9 @@ async function enviarMensagem() {
     const form = document.getElementById('form-contato')
     const dados = new FormData(form)
     const msgErro = document.getElementById('msg-erro')
+    const btn = document.getElementById('btn-enviar')
+    const spinner = document.getElementById('spinner')
+    const btnTexto = document.getElementById('btn-enviar-texto')
 
 
     // VERIFICAÇÃO SE TEM ALGUM CAMPO VAZIO
@@ -20,27 +23,47 @@ async function enviarMensagem() {
         return
     }
 
-    const resposta = await fetch('/contato', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            nome: dados.get('nome'),
-            email: dados.get('email'),
-            telefone: dados.get('telefone'),
-            assunto: dados.get('assunto'),
-            mensagem: dados.get('mensagem')
-        })
-        
-    })
+    btn.disabled = true
+    btnTexto.textContent = 'Enviando...'
+    spinner.style.display = 'inline-block'
 
-    const resultado = await resposta.json()
-    if (resultado.status === 'sucesso') {
-        alert('Mensagem enviada com sucesso!')
-        msgErro.innerText = ''
-        form.reset()
+    try{
+        const resposta = await fetch('/contato', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nome: dados.get('nome'),
+                email: dados.get('email'),
+                telefone: dados.get('telefone'),
+                assunto: dados.get('assunto'),
+                mensagem: dados.get('mensagem')
+            })
+            
+        })
+
+        const resultado = await resposta.json()
+        if (resultado.status === 'sucesso') {
+            btnTexto.textContent = 'Enviado!'
+            spinner.style.display = 'none'
+            msgErro.innerText = ''
+            form.reset()
+        }
+    } catch (erro){
+        btnTexto.textContent = 'Tente novamente'
+        spinner.style.display = 'none'
+        msgErro.innerText = erro
+    } finally{
+        setTimeout(() => {
+            btn.disabled = false
+            btnTexto.textContent = 'Enviar'
+            spinner.style.display = 'none'
+            msgErro.innerText = ''
+        }, 3000)
     }
+    
+
 }
 
 function digitar(event){
